8a598edc65dcc1eb7dd2aea290050e8b818905ee
shivamkapasia0 <kapasiashivam007@gmail.com>
2019-10-30 13:29:36 +0530

    Api Updates & UI.

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+const KTextFielInputDecoration =InputDecoration(
+    filled: true,
+    fillColor: Colors.white,
+    icon: Icon(Icons.location_city,color: Colors.white,),
+    hintText: 'Enter City name',hintStyle: TextStyle(color: Colors.black),
+    border: OutlineInputBorder(
+      borderRadius:BorderRadius.all(Radius.circular(10.0)),
+    )
+
+);
